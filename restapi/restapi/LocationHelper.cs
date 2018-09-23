af052f2141d7f8719daebf459910e841ffec47a4
Denis J. González S <dns.gonz@gmail.com>
2018-09-23 17:04:43 -0500

    Updated changes to services and util classes

---
+++ restapi/restapi/LocationHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restapi
+{
+    public static class LocationHelper
+    {
+        internal static (double latitude, double longitude) GetCurrentLocation(int id)
+        {
+
+            if (!Locations.ContainsKey(id))
+            {
+                Locations.Add(id, default((double latitude, double longitude)));
+            }
+
+            //This method updates the last known location for the car and simulates its movement
+            UpdateLocation(id);
+
+            return Locations[id];
+        }
+
+        private static readonly Random Randomizer = new Random();
+        private const double ValidLatitude = 90;
+        private const double ValidLongitude = 180;
+
+        private void UpdateLocation(int id)
+        {
+            (double latitude, double longitude) loc = Locations[id];
+
+            //If the default value is found, randomly assign a starting point.
+            if (loc.latitude == default(double) && loc.longitude == default(double))
+            {
+                Locations[id] = GetRandomStartingPoint();
+            }
+        }
+
+
+        private (double latitude, double longitude) GetRandomStartingPoint()
+        {
+            var lat = Randomizer.NextDouble() * ValidLatitude * (Randomizer.Next(2) > 0 ? -1 : 1);
+            var @long = Randomizer.NextDouble() * ValidLongitude * (Randomizer.Next(2) > 0 ? -1 : 1);
+
+            return (lat, @long);
+        }
+
+        private static readonly Dictionary<int, (double latitude, double longitude)> Locations = new Dictionary<int, (double latitude, double longitude)>();
+
+    }
+}
--- restapi/restapi/Startup.cs
+++ restapi/restapi/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace restapi
 {
@@ -24,6 +27,8 @@ namespace restapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            LoadJwtAuthorization(services);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,8 +39,46 @@ namespace restapi
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
+            app.UseAuthentication();
             app.UseMvc();
         }
+
+        internal static readonly byte[] JwtKey = Encoding.ASCII.GetBytes(@"this is a test key");
+
+        private void LoadJwtAuthorization(IServiceCollection services)
+        {
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = context =>
+                        {
+                            var userId = int.Parse(context.Principal.Identity.Name);
+                            if (userId == 0)
+                            {
+                                //Handle user validation against DB 
+                                context.Fail("Unauthorized");
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(JwtKey),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
+
     }
 }
