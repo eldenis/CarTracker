0c130e84b10cd6eea724a05b7c015ff69421fa24
Denis J. González S <dns.gonz@gmail.com>
2018-09-23 19:27:59 -0500

    Added references and main objects

---
+++ project/WpfApp/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using ReactiveUI;
+using System.Reactive.Linq;
+using System.Windows;
+
+namespace WpfApp
+{
+    public class MainViewModel : ReactiveObject
+    {
+
+        public MainViewModel()
+        {
+            UpdateCar = ReactiveCommand.Create(() =>
+            {
+                var parsedCorrectly = int.TryParse(NewCarToFollow, out int newCar);
+                NewCarToFollow = null;
+                if (!parsedCorrectly)
+                {
+                    MessageBox.Show("There was an error reading the number of the car to follow. Please, review it.",
+                        "", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                    return;
+                }
+
+                FollowedCar = newCar;
+            }, canExecute: this.WhenAnyValue(x => x.NewCarToFollow).Select(x => !string.IsNullOrWhiteSpace(x)));
+
+        }
+
+        private string _newCarToFollow;
+        public string NewCarToFollow
+        {
+            get => _newCarToFollow;
+            set => this.RaiseAndSetIfChanged(ref _newCarToFollow, value);
+        }
+
+        private int _followedCar = 1;
+        public int FollowedCar
+        {
+            get => _followedCar;
+            set => this.RaiseAndSetIfChanged(ref _followedCar, value);
+        }
+
+
+        private ReactiveCommand _updateCar;
+        public ReactiveCommand UpdateCar
+        {
+            get => _updateCar;
+            set => this.RaiseAndSetIfChanged(ref _updateCar, value);
+        }
+
+    }
+}
---
+++ project/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using ReactiveUI;
+using Splat;
+using System;
+using System.Reactive.Linq;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : IViewFor<MainViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = Locator.CurrentMutable.GetService<MainViewModel>();
+
+            //ViewModel.UpdateCar
+            //    .ObserveOn(RxApp.MainThreadScheduler)
+            //    .Catch<int, Exception>(ex =>
+            //    {
+            //        MessageBox.Show(ex.Message, "", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //        return null;
+            //    })
+            //    .Subscribe(newValue =>
+            //    {
+            //        ViewModel.FollowedCar = newValue;
+            //    });
+
+        }
+
+        /// <summary>
+        /// Allows the ViewModel to be used on the XAML via a dependency property
+        /// </summary>
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MainViewModel), typeof(MainWindow),
+                new PropertyMetadata(default(MainViewModel)));
+
+        /// <summary>
+        /// Implementation for the IViewFor interface
+        /// </summary>
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (MainViewModel)value;
+        }
+
+        /// <summary>
+        /// Regular property to use the ViewModel from this class
+        /// </summary>
+        public MainViewModel ViewModel
+        {
+            get => (MainViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+    }
+}
